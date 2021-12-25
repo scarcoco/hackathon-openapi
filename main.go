3de08c2c10adaf3cd93975288fee4b6987969a4b
scarcoco <cooxacom@163.com>
2021-12-25 22:13:01 +0800

    feat: add prometheus

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/scarcoco/hackathon-openapi/routers"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
+
 	r := gin.Default()
 
 	routers.Routers(r)
